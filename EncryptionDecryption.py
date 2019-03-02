11c66952f393ae076d3a140f995764e40df41b9c
Mohitshalout <38828457+Mohitshalout@users.noreply.github.com>
2019-03-02 18:30:10 +0530

    Update EncryptionDecryption.py

--- EncryptionDecryption.py
+++ EncryptionDecryption.py
@@ -10,6 +10,7 @@ def EncryptM(s,key):
         return
     else:
         li=[]
+        # adding pass-key with every character
         for i in range(0,len(s)):
             a = chr(ord(s[i])+key)
             li.append(a)
@@ -20,27 +21,21 @@ def EncryptM(s,key):
         return(s)
 #Decryption of a message
 def DecryptM(s,key):
-    if(len(s)<=0):
-        print("Message length is too short foe Encryption ...")
-        return
-    elif(len(str(key))<4):
-        print("Pass-Key is too short")
-        return
-    else:
-        li=[]
-        for i in range(0,len(s)):
-            a = chr(ord(s[i])-key)
-            li.append(a)
-        s=""
-        for i in range(0,len(li)):
-            s=s+li[i]
-        del li
-        return(s)
+    li=[]
+    # adding pass-key with every character
+    for i in range(0,len(s)):
+        a = chr(ord(s[i])-key)
+        li.append(a)
+    s=""
+    for i in range(0,len(li)):
+        s=s+li[i]
+    del li
+    return(s)
 #for take message from user with a pass-key of minimum length 4
 
 m = str(input("Enter your message here :\n"))
 k = int(input("enter a pass-key : "))
-E_m = EncryptM(m,k) 
-D_m = DecryptM(E_m,k)
-print(E_m)
-print(D_m)
+m = EncryptM(m,k) 
+D_m = DecryptM(m,k)
+print("Encrypted message is :\n",m)
+print("according to your key the Decrypted message is :\n",D_m)
